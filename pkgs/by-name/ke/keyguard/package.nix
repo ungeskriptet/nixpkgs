bf4db3c67258785f46a3b74b5499c96d54e18fe2
David Wronek <david.wronek@mainlining.org>
2026-08-13 17:27:21 +0200

    keyguard: 2.3.4 -> 3.0.2
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- pkgs/by-name/ke/keyguard/package.nix
+++ pkgs/by-name/ke/keyguard/package.nix
@@ -1,41 +1,33 @@
 {
   lib,
   stdenv,
+  rustPlatform,
   fetchFromGitHub,
   gradle_9,
-  jdk21,
-  fontconfig,
-  libxinerama,
-  libxrandr,
-  file,
-  gtk3,
-  glib,
-  cups,
-  lcms2,
-  alsa-lib,
+  jetbrains,
+  libx11,
+  cargo,
+  rustc,
+  protobuf,
+  symlinkJoin,
   makeDesktopItem,
   copyDesktopItems,
-  libglvnd,
-  autoPatchelfHook,
   writeText,
 }:
-
+let
+  jdk = jetbrains.jdk-21;
+  gradle = gradle_9.override { java = jdk; };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "keyguard";
-  version = "2.3.4";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "AChep";
     repo = "keyguard-app";
-    tag = "r20260201";
-    hash = "sha256-7n6/YgRzte2qsgQEfqppdpp5t8+xBjfOKjvNotAgJB0=";
+    tag = "r20260812.1";
+    hash = "sha256-lUyZk2qUl+ET7Pmvxn+5M7ihv/6xqMJi9K0+/lYMoFM=";
   };
-
-  postPatch = ''
-    substituteInPlace desktopLibJvm/build.gradle.kts \
-      --replace-fail 'resources.srcDir(rootDir.resolve("desktopLibNative/build/bin/universal"))' "" \
-      --replace-fail 'resourcesTask.dependsOn(":desktopLibNative:''${Tasks.compileNativeUniversal}")' ""
-  '';
 
   gradleBuildTask = ":desktopApp:createReleaseDistributable";
 
@@ -50,28 +42,56 @@ stdenv.mkDerivation (finalAttrs: {
     useBwrap = false;
   };
 
-  env.JAVA_HOME = jdk21;
+  env.JAVA_HOME = jdk;
 
-  gradleFlags = [ "-Dorg.gradle.java.home=${jdk21}" ];
+  gradleFlags = [
+    "-Pkeyguard.nativeCrypto.cargoOffline=true"
+    "-Dorg.gradle.java.home=${jdk}"
+  ];
+
+  cargoRoot = "util/crypto/rust";
+
+  cargoDeps = symlinkJoin {
+    name = "cargo-vendor-dir";
+    paths = [
+      (rustPlatform.fetchCargoVendor {
+        inherit (finalAttrs) cargoRoot version src;
+        pname = "crypto-rust";
+        hash = "sha256-xb51bzQH75YSwO25P7FShvywqcSbyHLmpF2c29TYGoM=";
+      })
+      (rustPlatform.fetchCargoVendor {
+        inherit (finalAttrs) version src;
+        pname = "desktopLibNative";
+        cargoRoot = "${finalAttrs.src}/desktopLibNative/src";
+        hash = "sha256-VxQk7eQPfM4iH65F6yY1AqgcFC3pzlLGxljyVWJKzb4=";
+      })
+      (rustPlatform.fetchCargoVendor {
+        inherit (finalAttrs) version src;
+        pname = "desktopSshAgent";
+        cargoRoot = "${finalAttrs.src}/desktopSshAgent/src";
+        hash = "sha256-dsrDouDQuPwobnecdHYfXBhAhRK0ckmyoVgWB4ZezGY=";
+      })
+      (rustPlatform.fetchCargoVendor {
+        inherit (finalAttrs) version src;
+        pname = "desktopGpgAgent";
+        cargoRoot = "${finalAttrs.src}/desktopGpgAgent/src";
+        hash = "sha256-HGx+5xkL6auib3gtyd6EQQXIAZCDLY0bhnBpeVppKPI=";
+      })
+    ];
+  };
 
   nativeBuildInputs = [
-    gradle_9
-    jdk21
+    gradle
+    jdk
+    cargo
+    protobuf
+    rustc
+    rustPlatform.cargoSetupHook
     copyDesktopItems
-    autoPatchelfHook
   ];
 
   buildInputs = [
-    fontconfig
-    libxinerama
-    libxrandr
-    file
-    gtk3
-    glib
-    cups
-    lcms2
-    alsa-lib
-    libglvnd
+    libx11
   ];
 
   doCheck = false;
@@ -101,7 +121,10 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/AChep/keyguard-app";
     mainProgram = "Keyguard";
     license = lib.licenses.unfree;
-    maintainers = with lib.maintainers; [ ilkecan ];
+    maintainers = with lib.maintainers; [
+      ilkecan
+      ungeskriptet
+    ];
     sourceProvenance = with lib.sourceTypes; [
       fromSource
       binaryBytecode
